cb11688718d61d28492d728bb74247cc30a111a0
SHENTongfei <116341224+SHENTongfei@users.noreply.github.com>
2024-05-17 14:41:26 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ from train_and_use import *
 # # --------------------------------------------------------------------------------------------
 
 # # If you have curated training data with AA and V_gene that you haven't used to train a model yet, use the following functions.
-# results = train_and_use_with_gene('Train_and_Test_Data/Data_for_train/Sample_A_V.csv',
+# result = train_and_use_with_gene('Train_and_Test_Data/Data_for_train/Sample_A_V.csv',
 #                                 'Train_and_Test_Data/X_test.csv',
 #                                 'Train_and_Test_Data/Y_test.csv',
 #                                 'Train_and_Test_Data/neg_Data/',
@@ -47,7 +47,7 @@ from train_and_use import *
 #                                 'Trained_DeepTAPE/DeepTAPE_A_V_new.h5')
 
 # # If you have curated training data with AA and V_gene_family that you haven't used to train a model yet, use the following functions.
-# results = train_and_use_with_gene_family('Train_and_Test_Data/Data_for_train/Sample_A_VF.csv',
+# result = train_and_use_with_gene_family('Train_and_Test_Data/Data_for_train/Sample_A_VF.csv',
 #                                         'Train_and_Test_Data/X_test.csv',
 #                                         'Train_and_Test_Data/Y_test.csv',
 #                                         'Train_and_Test_Data/neg_Data/',
@@ -55,14 +55,13 @@ from train_and_use import *
 #                                         cnn_lstm_res_gene,
 #                                         'Trained_DeepTAPE/DeepTAPE_A_VF_new.h5')
 
-
- # results = train_and_use('Train_and_Test_Data/Data_for_train/Sample_A.csv',
- #                        'Train_and_Test_Data/X_test.csv',
- #                        'Train_and_Test_Data/Y_test.csv',
- #                        'Train_and_Test_Data/neg_Data/',
- #                        'Train_and_Test_Data/pos_Data/',
- #                          cnn_lstm_res,
- #                          'Trained_DeepTAPE/DeepTAPE_A_new.h5')
+# result = train_and_use('Train_and_Test_Data/Data_for_train/Sample_A.csv',
+#                     'Train_and_Test_Data/X_test.csv',
+#                     'Train_and_Test_Data/Y_test.csv',
+#                     'Train_and_Test_Data/neg_Data/',
+#                     'Train_and_Test_Data/pos_Data/',
+#                       cnn_lstm_res,
+#                       'Trained_DeepTAPE/DeepTAPE_A_new.h5')
 
 
 
